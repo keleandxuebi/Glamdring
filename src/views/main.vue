3bbebf2dc862c7051186334c511c34b73fdf66d0
shangboyang <yang8701@gmail.com>
2016-04-26 17:13:47 +0800

    dd

--- src/views/main.vue
+++ src/views/main.vue
@@ -5,7 +5,7 @@
         <h1 class="title" v-text="message" v-on:click="greet"></h1>
         -->
         <!-- use v-link directive for navigation. -->
-        <p class="main">
+        <div class="main">
             <a v-link="'home'">Home</a>
             <!-- if route View is onload .v-link-active will be inject -->
             <!-- and the classname also can be rename  -->
@@ -13,10 +13,8 @@
             <!-- 具名路由 同时支持 router.go({name: 'one_piece', params: {barID: 001}) -->
             <a v-link="{ name: 'one_piece', params: {barID: '001'} }">Go to Bar: One Piece</a>
             <a v-link="{ path: '/user/boyang' }">Go to User: Boyang</a>
-        </p>
-        <p>
             <a v-link="{ name: 'mySpace'}">My Space</a>
-        </p>
+        </div>
         <!-- use router-view element as route outlet -->
         <!-- important -->
         <router-view class="view" transition="expand" transition-mode="out-in"></router-view>
@@ -36,6 +34,10 @@ export default {
 .main {
     position: relative;
     top: 44px;
+}
+.main a {
+    display: block;
+    height: 25px; width: 100%;
 }
 /* 必需 */
 .expand-transition {
