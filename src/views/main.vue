8e20d84c7d2b126c139a67809fee99117b7885ec
shangboyang <yang8701@gmail.com>
2016-06-03 22:35:44 +0800

    update

--- src/views/main.vue
+++ src/views/main.vue
@@ -34,6 +34,10 @@ export default {
     width: 100%; height: 100%;
     z-index: 1;
 }
+.main a {
+    display: block;
+    height: 25px; width: 100%;
+}
 .view {
     position: absolute; top: 0; left: 0;
     width: 100%; height: 100%;
